72cea4f39936d1a28060d4cf574e52101b5e502c
andrewmoores <a.s.moores@gmail.com>
2016-03-17 08:51:05 +0000

    [AM] Renamed package to a uk.gov one.  Modified loader class to scan all files on classpath and load into appropriate collection.  Added some more test data.

---
+++ src/main/java/uk/gov/digital/ho/proving/income/datastore/mongobd/Mongo.java
@@ -0,0 +1,60 @@
+package uk.gov.digital.ho.proving.income.datastore.mongobd;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import org.apache.commons.io.IOUtils;
+import org.bson.Document;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+
+public class Mongo {
+
+    public static void main(String args[]) throws IOException {
+        ResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
+
+        MongoCollection<Document> applicants = new MongoClient().getDatabase("test").getCollection("applicants");
+        applicants.drop();
+
+        Resource[] mappingLocations = patternResolver.getResources("classpath*:applicant*.json");
+
+        for (Resource mappingLocation : mappingLocations) {
+            String document = null;
+            try {
+                document = IOUtils.toString(mappingLocation.getInputStream());
+            } catch (IOException e) {
+                System.out.println("Error loading json from classpath: " + e);
+            }
+
+            System.out.println("Adding document from: " + mappingLocation.getURI());
+
+            applicants.insertOne(Document.parse(document));
+        }
+
+        System.out.println("Applicants collection now contains: " + applicants.count());
+
+        MongoCollection<Document> applications = new MongoClient().getDatabase("test").getCollection("applications");
+        applications.drop();
+
+        mappingLocations = patternResolver.getResources("classpath*:application*.json");
+
+        for (Resource mappingLocation : mappingLocations) {
+            String document = null;
+            try {
+                document = IOUtils.toString(mappingLocation.getInputStream());
+            } catch (IOException e) {
+                System.out.println("Error loading json from classpath: " + e);
+            }
+
+            System.out.println("Adding document from: " + mappingLocation.getURI());
+
+            applications.insertOne(Document.parse(document));
+        }
+
+        System.out.println("Applicants collection now contains: " + applications.count());
+    }
+}
