236b84c481ebeeddfa8a6600c9e43b3deee2d154
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-06-10 13:50:35 +0100

    added ability to set Mongodb host and port using environment variables

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This project creates some test data in a MongoDB instance
 Start mongo as per usual:
 
  mongod --dpath <path to your data directoy>
+ 
+ optional - override Mongodb host and port with environment variables  - MONGO_HOST and MONGO_PORT
--- src/main/java/uk/gov/digital/ho/proving/income/datastore/mongobd/Mongo.java
+++ src/main/java/uk/gov/digital/ho/proving/income/datastore/mongobd/Mongo.java
@@ -14,10 +14,13 @@ import java.util.stream.IntStream;
 
 public class Mongo {
 
+    private static String MONGO_HOST_ENV_NAME = "MONGO_HOST";
+    private static String MONGO_PORT_ENV_NAME = "MONGO_PORT";
+
     public static void main(String args[]) throws IOException {
         ResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
 
-        MongoCollection<Document> applicants = new MongoClient("pttg-test-mongodb").getDatabase("test").getCollection("applicants");
+        MongoCollection<Document> applicants = getMongoClient().getDatabase("test").getCollection("applicants");
         //MongoCollection<Document> applicants = new MongoClient("localhost", 8888).getDatabase("test").getCollection("applicants");
         applicants.drop();
 
@@ -38,7 +41,7 @@ public class Mongo {
 
         System.out.println("Applicants collection now contains: " + applicants.count());
 
-        MongoCollection<Document> applications = new MongoClient("pttg-test-mongodb").getDatabase("test").getCollection("applications");
+        MongoCollection<Document> applications = getMongoClient().getDatabase("test").getCollection("applications");
         //MongoCollection<Document> applications = new MongoClient("localhost", 8888).getDatabase("test").getCollection("applications");
         applications.drop();
 
@@ -58,5 +61,25 @@ public class Mongo {
         }
 
         System.out.println("Applicants collection now contains: " + applications.count());
+    }
+
+    /*quick hack to allow overriding by environment variables*/
+    private static MongoClient getMongoClient() {
+        final String host = System.getenv(MONGO_HOST_ENV_NAME);
+        final String port = System.getenv(MONGO_PORT_ENV_NAME);
+        boolean useHost = (host != null && !host.isEmpty());
+        boolean usePort = (port != null && !port.isEmpty());
+        MongoClient client = null;
+        if (useHost) {
+            if (usePort) {
+                client = new MongoClient(host, Integer.parseInt(port));
+            } else {
+                client = new MongoClient(host);
+            }
+        } else {
+            client = new MongoClient();
+        }
+        System.out.println("MongoClient invoked using host[" + host + "] and port [" + port + "]");
+        return client;
     }
 }
